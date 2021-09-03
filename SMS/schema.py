e9d7277d68f41a390b5b07dd1ed336d69d2504d8
judeakinwale <judeakinwale@gmail.com>
2021-09-03 10:53:15 +0100

    Update documentation schema

--- SMS/schema.py
+++ SMS/schema.py
@@ -5,12 +5,12 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      title="School Management System API",
+      default_version='v0.5',
+      description="School management system",
+      # terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="judeakinwale@gmail.com"),
+      license=openapi.License(name="MIT License"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
