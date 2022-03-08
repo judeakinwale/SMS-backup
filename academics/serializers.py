5b50f54a0e3be685f8793a4f5f8079e707793cf0
judeakinwale <judeakinwale@gmail.com>
2022-03-08 21:41:25 +0100

    Implement serializer nesting in academics app

--- academics/serializers.py
+++ academics/serializers.py
@@ -3,123 +3,6 @@ from rest_framework import serializers
 from academics import models
 
 
-class FacultySerializer(serializers.HyperlinkedModelSerializer):
-    """serializer for the Faculty model"""
-
-    dean = serializers.HyperlinkedRelatedField(
-        queryset=get_user_model().objects.filter(is_staff=True),
-        view_name='user:user-detail',
-        allow_null=True,
-        required=False,
-    )
-
-    class Meta:
-        model = models.Faculty
-        fields = [
-            'id',
-            'url',
-            'name',
-            'code',
-            'description',
-            'dean',
-            'is_active',
-        ]
-        extra_kwargs = {
-            'url': {'view_name': 'academics:faculty-detail'}
-        }
-
-
-class DepartmentSerializer(serializers.HyperlinkedModelSerializer):
-    """serializer for the Department model"""
-
-    faculty = serializers.HyperlinkedRelatedField(
-        queryset=models.Faculty.objects.all(),
-        view_name='academics:faculty-detail',
-    )
-    head = serializers.HyperlinkedRelatedField(
-        queryset=get_user_model().objects.filter(is_staff=True),
-        view_name='user:user-detail',
-        allow_null=True,
-        required=False
-    )
-
-    class Meta:
-        model = models.Department
-        fields = [
-            'id',
-            'url',
-            'faculty',
-            'name',
-            'code',
-            'description',
-            'head',
-            'is_active',
-        ]
-        extra_kwargs = {
-            'url': {'view_name': 'academics:department-detail'}
-        }
-
-
-class SpecializationSerializer(serializers.HyperlinkedModelSerializer):
-    """serializer for the Specialization model"""
-
-    department = serializers.HyperlinkedRelatedField(
-        queryset=models.Department.objects.all(),
-        view_name='academics:department-detail',
-    )
-    max_level = serializers.HyperlinkedRelatedField(
-        queryset=models.Level.objects.all(),
-        view_name='academics:level-detail',
-    )
-
-    class Meta:
-        model = models.Specialization
-        fields = [
-            'id',
-            'url',
-            'department',
-            'name',
-            'code',
-            'max_level',
-            'description',
-            'is_active',
-        ]
-        extra_kwargs = {
-            'url': {'view_name': 'academics:specialization-detail'}
-        }
-
-
-class CourseSerializer(serializers.HyperlinkedModelSerializer):
-    """serializer for the Course model"""
-
-    specialization = serializers.HyperlinkedRelatedField(
-        queryset=models.Specialization.objects.all(),
-        view_name='academics:specialization-detail',
-    )
-    coordinator = serializers.HyperlinkedRelatedField(
-        queryset=get_user_model().objects.filter(is_staff=True),
-        view_name='user:user-detail',
-        allow_null=True,
-        required=False,
-    )
-
-    class Meta:
-        model = models.Course
-        fields = [
-            'id',
-            'url',
-            'specialization',
-            'name',
-            'code',
-            'description',
-            'coordinator',
-            'is_active',
-        ]
-        extra_kwargs = {
-            'url': {'view_name': 'academics:course-detail'}
-        }
-
-
 class LevelSerializer(serializers.HyperlinkedModelSerializer):
     """serializer for the Level model"""
 
@@ -211,3 +94,128 @@ class RecommendedCoursesSerializer(serializers.HyperlinkedModelSerializer):
             'url': {'view_name': 'academics:recommendedcourses-detail'}
         }
         depth = 1
+
+
+class CourseSerializer(serializers.HyperlinkedModelSerializer):
+    """serializer for the Course model"""
+
+    specialization = serializers.HyperlinkedRelatedField(
+        queryset=models.Specialization.objects.all(),
+        view_name='academics:specialization-detail',
+    )
+    coordinator = serializers.HyperlinkedRelatedField(
+        queryset=get_user_model().objects.filter(is_staff=True),
+        view_name='user:user-detail',
+        allow_null=True,
+        required=False,
+    )
+
+    class Meta:
+        model = models.Course
+        fields = [
+            'id',
+            'url',
+            'specialization',
+            'name',
+            'code',
+            'description',
+            'coordinator',
+            'is_active',
+        ]
+        extra_kwargs = {
+            'url': {'view_name': 'academics:course-detail'}
+        }
+
+
+class SpecializationSerializer(serializers.HyperlinkedModelSerializer):
+    """serializer for the Specialization model"""
+
+    department = serializers.HyperlinkedRelatedField(
+        queryset=models.Department.objects.all(),
+        view_name='academics:department-detail',
+    )
+    max_level = serializers.HyperlinkedRelatedField(
+        queryset=models.Level.objects.all(),
+        view_name='academics:level-detail',
+    )
+    courses = CourseSerializer(source='course_set', many=True, read_only=True)
+    recommended_courses = RecommendedCoursesSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = models.Specialization
+        fields = [
+            'id',
+            'url',
+            'department',
+            'name',
+            'code',
+            'max_level',
+            'description',
+            'courses',
+            'recommended_courses',
+            'is_active',
+        ]
+        extra_kwargs = {
+            'url': {'view_name': 'academics:specialization-detail'}
+        }
+
+
+class DepartmentSerializer(serializers.HyperlinkedModelSerializer):
+    """serializer for the Department model"""
+
+    faculty = serializers.HyperlinkedRelatedField(
+        queryset=models.Faculty.objects.all(),
+        view_name='academics:faculty-detail',
+    )
+    head = serializers.HyperlinkedRelatedField(
+        queryset=get_user_model().objects.filter(is_staff=True),
+        view_name='user:user-detail',
+        allow_null=True,
+        required=False
+    )
+    specializations = SpecializationSerializer(source='specialization_set', many=True, read_only=True)
+
+    class Meta:
+        model = models.Department
+        fields = [
+            'id',
+            'url',
+            'faculty',
+            'name',
+            'code',
+            'description',
+            'head',
+            'specializations',
+            'is_active',
+        ]
+        extra_kwargs = {
+            'url': {'view_name': 'academics:department-detail'}
+        }
+
+
+class FacultySerializer(serializers.HyperlinkedModelSerializer):
+    """serializer for the Faculty model"""
+
+    dean = serializers.HyperlinkedRelatedField(
+        queryset=get_user_model().objects.filter(is_staff=True),
+        view_name='user:user-detail',
+        allow_null=True,
+        required=False,
+    )
+    departments = DepartmentSerializer(source='department_set', many=True, read_only=True)
+
+    class Meta:
+        model = models.Faculty
+        fields = [
+            'id',
+            'url',
+            'name',
+            'code',
+            'description',
+            'dean',
+            'departments',
+            'is_active',
+        ]
+        extra_kwargs = {
+            'url': {'view_name': 'academics:faculty-detail'}
+        }
