9c603433ae3bfac870875a6736034f46738ebc64
judeakinwale <judeakinwale@gmail.com>
2022-03-15 08:46:35 +0100

    Update reset password email content, move logic to user.views

--- core/views.py
+++ core/views.py
@@ -1,52 +1,53 @@
-# from django.http import HttpResponse
-# from django.shortcuts import render
-from django.conf import settings
-from django.core.mail import EmailMultiAlternatives
-from django.dispatch import receiver
-from django.template.loader import render_to_string
-from django.urls import reverse
+# from django.conf import settings
+# from django.core.mail import EmailMultiAlternatives
+# from django.dispatch import receiver
+# from django.template.loader import render_to_string
+# from django.urls import reverse
+# from django.contrib.auth import get_user_model
 
-from django_rest_passwordreset.signals import reset_password_token_created
+# from django_rest_passwordreset.signals import reset_password_token_created
 
 
-@receiver(reset_password_token_created)
-def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
-    """
-    Handles password reset tokens
-    When a token is created, an e-mail needs to be sent to the user
-    :param sender: View Class that sent the signal
-    :param instance: View Instance that sent the signal
-    :param reset_password_token: Token Model Object
-    :param args:
-    :param kwargs:
-    :return:
-    """
-    try:
-        # send an e-mail to the user
-        context = {
-            'current_user': reset_password_token.user,
-            'username': reset_password_token.user.first_name,
-            'email': reset_password_token.user.email,
-            'reset_password_url': "{}?token={}".format(
-                instance.request.build_absolute_uri(reverse('password_reset:reset-password-confirm')),
-                reset_password_token.key)
-        }
+# @receiver(reset_password_token_created)
+# def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+#     """
+#     Handles password reset tokens
+#     When a token is created, an e-mail needs to be sent to the user
+#     :param sender: View Class that sent the signal
+#     :param instance: View Instance that sent the signal
+#     :param reset_password_token: Token Model Object
+#     :param args:
+#     :param kwargs:
+#     :return:
+#     """
+#     try:
+#         # send an e-mail to the user
+#         print(reset_password_token)
+#         context = {
+#             'current_user': reset_password_token.user,
+#             'username': reset_password_token.user.first_name,
+#             'email': reset_password_token.user.email,
+#             'reset_password_url': "{}?token={}".format(
+#                 instance.request.build_absolute_uri(reverse('password_reset:reset-password-confirm')),
+#                 reset_password_token.key)
+#         }
 
-        # render email text
-        email_html_message = render_to_string('email/user_reset_password.html', context)
-        email_plaintext_message = render_to_string('email/user_reset_password.txt', context)
+#         # render email text
+#         email_html_message = render_to_string('email/user_reset_password.html', context)
+#         email_plaintext_message = render_to_string('email/user_reset_password.txt', context)
 
-        msg = EmailMultiAlternatives(
-            # title:
-            "Password Reset for {title}".format(title="SMS Application"),
-            # message:
-            email_plaintext_message,
-            # from:
-            settings.EMAIL_HOST_USER  # "noreply@somehost.local",
-            # to:
-            [reset_password_token.user.email]
-        )
-        msg.attach_alternative(email_html_message, "text/html")
-        msg.send()
-    except Exception as e:
-        print(e)
+#         msg = EmailMultiAlternatives(
+#             # title:
+#             "Password Reset for {title}".format(title="School Management Portal"),
+#             # message:
+#             email_plaintext_message,
+#             # from:
+#             settings.EMAIL_HOST_USER  # "noreply@somehost.local",
+#             # to:
+#             [reset_password_token.user.email]
+#         )
+#         msg.attach_alternative(email_html_message, "text/html")
+#         msg.send()
+#         print("Mail successfully sent")
+#     except Exception as e:
+#         print(f"There was an error sending mail: {e}")
