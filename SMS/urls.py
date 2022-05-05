29c4760452b633a0efdab672185acbe08019a475
judeakinwale <judeakinwale@gmail.com>
2022-05-05 08:59:20 +0100

    Fix media config

--- SMS/urls.py
+++ SMS/urls.py
@@ -34,3 +34,5 @@ urlpatterns = [
     # For django-rest-passwordreset
     path("password_reset/", include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
