33c59f11bde8fa38826d84ec68e1a877b0b696ab
judeakinwale <judeakinwale@gmail.com>
2022-06-07 22:57:38 +0100

    Minor changes

--- information/models.py
+++ information/models.py
@@ -50,7 +50,8 @@ class Notice(models.Model):
         
     def save(self, *args, **kwargs):
         notice = super(Notice, self).save(*args, **kwargs)
-        
+        print('created notice')
+        print(notice)
         related_students = utils.get_related_students(notice.scope)
         staff_notice_email = utils.send_staff_notice_email(notice)
         notice_email = utils.send_student_notice_email(notice)
