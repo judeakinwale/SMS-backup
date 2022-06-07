132fa0c38202a035c4949c2fe51463ba6e5582ef
judeakinwale <judeakinwale@gmail.com>
2022-06-07 22:51:51 +0100

    Fix bug with notice

--- information/models.py
+++ information/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
+from information import utils
 from academics import models as amodels
 
 # Create your models here.
