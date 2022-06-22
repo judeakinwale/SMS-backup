139542826aba6ece1ecf7c6cf0b464b9b208e445
judeakinwale <judeakinwale@gmail.com>
2022-06-22 09:11:21 +0100

    Minor changes

--- SMS/settings.py
+++ SMS/settings.py
@@ -187,6 +187,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     "EXCEPTION_HANDLER": "SMS.utils.api_exception_handler",
+    # 'EXCEPTION_HANDLER':'rest_framework.views.exception_handler',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
     'DEFAULT_FILTER_BACKENDS': (
